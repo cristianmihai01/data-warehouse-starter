32fd9fc8c147c2ed03ad3f11784b2914da212464
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-08-26 10:51:42 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
 
 // import animate on scroll
-import Aos from 'aos';
-import 'aos/dist/aos.css';
 
 // import components
-import Hero from './components/Hero';
-import About from './components/About';
-import Features from './components/Features';
-import Testimonials from './components/Testimonials';
-import CtaSection from './components/CtaSection';
-import Footer from './components/Footer';
 
 const App = () => {
-  // animate on scroll initialization
-  Aos.init({
-    duration: 1800,
-    offset: 0,
-  });
   return <div className='overflow-hidden'>react app</div>;
 };
 
