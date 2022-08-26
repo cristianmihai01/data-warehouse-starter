04929c1908a6b906126e2362fa570fb2c4b0bf1b
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-08-26 09:54:05 +0300

    added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,7 @@
 import React from 'react';
 
-// import navigation data
-import { navigationData } from '../data';
-
 const Nav = () => {
-  return (
-    <nav>
-      <ul className='flex gap-x-8'>
-        {navigationData.map((item, index) => {
-          return (
-            <li key={index}>
-              <a href={item.href}>{item.name}</a>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
-  );
+  return <div>Nav</div>;
 };
 
 export default Nav;
--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,22 +1,7 @@
 import React from 'react';
 
-// import navigation data
-import { navigationData } from '../data';
-
 const NavMobile = () => {
-  return (
-    <ul className='flex flex-col px-6 py-8 gap-y-4'>
-      {navigationData.map((item, index) => {
-        return (
-          <li key={index}>
-            <a className='text-white' href={item.href}>
-              {item.name}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  );
+  return <div>NavMobile</div>;
 };
 
 export default NavMobile;
